6f2a6245a4a2d935fb9bf0d2ec990f12d56ede0d
emyferreira <emily.ferreira@sptech.school>
2024-10-09 19:00:21 -0300

    alterando tamaho dos gráficos

--- src/pages/dashcondominio/DashCondominioPage.jsx
+++ src/pages/dashcondominio/DashCondominioPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Row, Col, Button } from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 import api from '../../api/api'
 import './DashCondominio.module.css'
 import GraficoLinhaComparativo from '../../components/graficolinhacomparativo/GraficoLinhaComparativo'
@@ -77,42 +77,38 @@ const DashCondominioPage = () => {
   return (
     <>
       <div style={{ display: "block", height: "100%", marginBottom: "100px" }}>
-        
-        {/* <Container> */}
         <Col md lg={10} style={{ marginTop: "100px" }}>
           <Row>
-
             <CardScrt legenda="Selecione o Condomínio" info={dadosCestasProduzidas.count} bgColor="#D3D3D3" />
             <CardScrt legenda="Total de Alimentos Arrecadados" info={dadosEstoque.length > 0 ? dadosEstoque[dadosEstoque.length - 1].count : 0} bgColor="#5FED6D" />
             <CardScrt legenda="Produtos não conformes" info={dadosPizza[1]} bgColor="#FDEA3C" />
             <CardScrt legenda="Total de Alimentos Vencidos" info={somaCountDadosVencidos} bgColor="#ED8686" />
           </Row>
           <Row>
-            <Col md lg={12}>
+            <Col md lg={6}>
               <div>
                 <GraficoLinha data={dadosEstoque} cores={'#22CC52'} titulo={'Quantidade total de alimentos arrecadados nos condomínios'} label={'Quantidade'} />
+              </div>
+            </Col>
+            <Col md lg={6}>
+              <div>
                 <GraficoLinhaComparativo data={dadosVencidosPorMes} cores={'#FF5555'} titulo={'Quantidade de doações variadas por condomínios'} label={'Quantidade'} />
               </div>
             </Col>
           </Row>
           <Row>
-            <Col md lang={12}>
+            <Col md lang={6}>
               <div>
                 <GraficoBarra data={dadosEstoque} cores={'#22CC52'} titulo={'Quantidade de produto por condomínio'} label={'Quantidade'} />
               </div>
             </Col>
-          </Row>
-          <Row>
-            <Col md lang={12}>
+            <Col md lang={6}>
               <div style={{ border: "1px solid #0005", marginTop: "10px", padding: "10px", height: "100%" }}>
                 <ListaBarraProgresso titulo={"Análise de alimentos por condomínio"} itens={dadosArrecadadosXVencidos}/>
               </div>
             </Col>
           </Row>
-          <Row>
-          </Row>
         </Col>
-        {/* </Container> */}
       </div>
     </>
   );
