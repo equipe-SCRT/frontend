cc26ca95e2c3c47db5fea6d6ccf482892e994ed4
brunamatuciak <bruna.guedes@sptech.school>
2024-10-26 23:48:26 -0300

    feat: atualizar e excluir

--- src/pages/tipo-cesta/TipoCestaCadastroPage.jsx
+++ src/pages/tipo-cesta/TipoCestaCadastroPage.jsx
@@ -3,6 +3,9 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import "./TipoCestaPage.module.css"
 import Swal from 'sweetalert2';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+import { Button } from 'primereact/button';
 
 const TipoCestaCadastro = () => {
   const [getNomeProdutos, setNomeProdutos] = useState([]);
@@ -12,6 +15,9 @@ const TipoCestaCadastro = () => {
   const [getTipoCestaId, setTipoCestaId] = useState(0);
   const [getProdutos, setProdutos] = useState([])
   const [getNomeProdutoLista, setNomeProdutoLista] = useState("")
+  const [getTipoCestas, setTipoCestas] = useState([]);
+  const [editMode, setEditMode] = useState(false);
+  const [editedRowData, setEditedRowData] = useState(null);
 
   
   useEffect(() => {
@@ -26,6 +32,21 @@ const TipoCestaCadastro = () => {
       'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
     }
   });
+
+  useEffect(() => {
+    async function handleTipoCestas(){
+      try{
+        const response = await api.get("/tipos-cestas")
+        setTipoCestas(response.data)
+        console.log(response.data)
+      }catch(error){
+        console.log(error)
+      }
+    }
+      handleTipoCestas();
+    }, []);
+
+  
 
   async function handleCadastroTipoCestaProduto(){
     const tipoCesta = {
@@ -49,8 +70,12 @@ const TipoCestaCadastro = () => {
         produto: getProdutos[i],
         idTipoCesta: getTipoCestaId
       }
-      
-      const response = await api.post("produto-cestas", produtoCesta)
+
+      try{
+        await api.post("produto-cestas", produtoCesta)
+      }catch(error){
+
+      }
     }
   }
 
@@ -81,6 +106,231 @@ const TipoCestaCadastro = () => {
       } 
       setProdutos(prevLista => [...prevLista, produtos]);
   }
+
+  const renderEditableCell = (rowData, field) => {
+    console.log(`${Object.keys(rowData)}`)
+    if (editMode && rowData.id === editedRowData?.id) {
+      if (field == "id") {
+        return (
+          <>
+              <td key={field}>
+                  <input
+                      type="text"
+                      defaultValue={rowData[field]}
+                      onChange={(e) => {
+                          setEditedRowData({ ...editedRowData, [field]: e.target.value });
+                      }}
+                      disabled
+                  />
+              </td>
+          </>
+      );
+      }else{
+        return (
+            <>
+                <td key={field}>
+                    <input
+                        type="text"
+                        defaultValue={rowData[field]}
+                        onChange={(e) => {
+                            setEditedRowData({ ...editedRowData, [field]: e.target.value });
+                        }}
+                    />
+                </td>
+            </>
+        );
+      }
+    }
+
+    return (
+        <>
+            <td key={field}>{rowData[field]}</td>
+        </>
+    );
+};
+
+const handleCancelClick = (rowData) => {
+  setEditMode(false);
+  setEditedRowData(rowData);
+};
+
+const renderActionCell = (rowData) => {
+  return (
+    <>
+      {editMode && rowData.id === editedRowData?.id ? (
+        <>
+          <Button icon={
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><g fill="none" fill-rule="evenodd"><path d="m12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035q-.016-.005-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427q-.004-.016-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093q.019.005.029-.008l.004-.014l-.034-.614q-.005-.018-.02-.022m-.715.002a.02.02 0 0 0-.027.006l-.006.014l-.034.614q.001.018.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z" /><path fill="#000" d="M6 2a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6.414A2 2 0 0 0 19.414 5L17 2.586A2 2 0 0 0 15.586 2zm0 2h9.586L18 6.414V20H6zm10.238 6.793a1 1 0 1 0-1.414-1.414l-4.242 4.243l-1.415-1.415a1 1 0 0 0-1.414 1.414l2.05 2.051a1.1 1.1 0 0 0 1.556 0l4.88-4.879Z" /></g></svg>
+          } onClick={() => handleAlterar(editedRowData)} className="btn" />
+          <Button icon={
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path fill="#FF4444" d="m8.4 17l3.6-3.6l3.6 3.6l1.4-1.4l-3.6-3.6L17 8.4L15.6 7L12 10.6L8.4 7L7 8.4l3.6 3.6L7 15.6zm3.6 5q-2.075 0-3.9-.788t-3.175-2.137T2.788 15.9T2 12t.788-3.9t2.137-3.175T8.1 2.788T12 2t3.9.788t3.175 2.137T21.213 8.1T22 12t-.788 3.9t-2.137 3.175t-3.175 2.138T12 22m0-2q3.35 0 5.675-2.325T20 12t-2.325-5.675T12 4T6.325 6.325T4 12t2.325 5.675T12 20m0-8" /></svg>
+          } onClick={() => handleCancelClick(rowData)} className="btn" />
+        </>
+      ) : (
+        <>
+          <Button icon={
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 36 36"><path fill="#000" d="M33.87 8.32L28 2.42a2.07 2.07 0 0 0-2.92 0L4.27 23.2l-1.9 8.2a2.06 2.06 0 0 0 2 2.5a2 2 0 0 0 .43 0l8.29-1.9l20.78-20.76a2.07 2.07 0 0 0 0-2.92M12.09 30.2l-7.77 1.63l1.77-7.62L21.66 8.7l6 6ZM29 13.25l-6-6l3.48-3.46l5.9 6Z" class="clr-i-outline clr-i-outline-path-1" /><path fill="none" d="M0 0h36v36H0z" /></svg>
+          } onClick={() => handleEditClick(rowData)} className="btn" />
+          <Button icon={
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path fill="none" stroke="#FF4444" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 7h16M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2l1-12M9 7V4a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v3m-5 5l4 4m0-4l-4 4" /></svg>
+          }
+            onClick={() => handleDelete(rowData.id)}
+            className="btn" />
+        </>
+      )}
+    </>
+  );
+};
+
+const handleEditClick = (rowData) => {
+  setEditMode(true);
+  setEditedRowData(rowData);
+};
+
+const handleDelete = (id) => {
+  if (sessionStorage.getItem('userId') === id) {
+    const Toast = Swal.mixin({
+      toast: true,
+      position: "top-end",
+      showConfirmButton: false,
+      timer: 3000,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.onmouseenter = Swal.stopTimer;
+        toast.onmouseleave = Swal.resumeTimer;
+      }
+    });
+    Toast.fire({
+      icon: "error",
+      title: "Atenção! Você não pode deletar a si mesmo!"
+    });
+  } else {
+    api.delete("/tipos-cestas/" + id)
+      .then((response) => {
+        if (response.status === 204) {
+
+          setTimeout(() => {
+            window.location.reload();
+          }, 1000);
+
+          const Toast = Swal.mixin({
+            toast: true,
+            position: "top-end",
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+              toast.onmouseenter = Swal.stopTimer;
+              toast.onmouseleave = Swal.resumeTimer;
+            }
+          });
+          Toast.fire({
+            icon: "success",
+            title: "Usuário excluido com sucesso!"
+          });
+        } else {
+          const Toast = Swal.mixin({
+            toast: true,
+            position: "top-end",
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+              toast.onmouseenter = Swal.stopTimer;
+              toast.onmouseleave = Swal.resumeTimer;
+            }
+          });
+          Toast.fire({
+            icon: "error",
+            title: "Erro ao excluir usuário!"
+          });
+        }
+      }
+      ).catch(() => {
+        const Toast = Swal.mixin({
+          toast: true,
+          position: "top-end",
+          showConfirmButton: false,
+          timer: 3000,
+          timerProgressBar: true,
+          didOpen: (toast) => {
+            toast.onmouseenter = Swal.stopTimer;
+            toast.onmouseleave = Swal.resumeTimer;
+          }
+        });
+        Toast.fire({
+          icon: "error",
+          title: "Erro ao excluir usuário!"
+        });
+      })
+  }
+}
+
+const handleAlterar = (rowData) => {
+  const id = rowData.id
+
+  var tipoCestaAlterada = {
+    nome: rowData.nome
+  }
+
+  api.put(`/tipos-cestas/${id}`, tipoCestaAlterada)
+    .then((response) => {
+      if (response.status === 200) {
+
+        setEditMode(false);
+
+        const Toast = Swal.mixin({
+          toast: true,
+          position: "top-end",
+          showConfirmButton: false,
+          timer: 3000,
+          timerProgressBar: true,
+          didOpen: (toast) => {
+            toast.onmouseenter = Swal.stopTimer;
+            toast.onmouseleave = Swal.resumeTimer;
+          }
+        });
+        Toast.fire({
+          icon: "success",
+          title: "Usuário atualizado com sucesso!"
+        });
+        window.location.reload()
+      } else {
+        const Toast = Swal.mixin({
+          toast: true,
+          position: "top-end",
+          showConfirmButton: false,
+          timer: 3000,
+          timerProgressBar: true,
+          didOpen: (toast) => {
+            toast.onmouseenter = Swal.stopTimer;
+            toast.onmouseleave = Swal.resumeTimer;
+          }
+        });
+        Toast.fire({
+          icon: "error",
+          title: "Erro ao atualizar usuário!"
+        });
+      }
+    }
+    ).catch(() => {
+      const Toast = Swal.mixin({
+        toast: true,
+        position: "top-end",
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.onmouseenter = Swal.stopTimer;
+          toast.onmouseleave = Swal.resumeTimer;
+        }
+      });
+      Toast.fire({
+        icon: "error",
+        title: "Erro ao atualizar usuário!"
+      });
+    })
+};
 
   return (
     <>
@@ -131,7 +381,7 @@ const TipoCestaCadastro = () => {
         <div className="card-body" style={{ border: '1px solid #DDE1E6', backgroundColor: '# f9f9f9' }}>
           <p className="card-description">Listagem</p>
           <div className="table-responsive">
-            <table className="table table-striped">
+            {/* <table className="table table-striped">
               <thead>
                 <th># <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M440-800v487L216-537l-56 57 320 320 320-320-56-57-224 224v-487h-80Z" /></svg></th>
                 <th> Nome <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M440-800v487L216-537l-56 57 320 320 320-320-56-57-224 224v-487h-80Z" /></svg> </th>
@@ -141,7 +391,12 @@ const TipoCestaCadastro = () => {
               </thead>
               <tbody>
               </tbody>
-            </table>
+            </table> */}
+            <DataTable value={getTipoCestas} tableStyle={{ minWidth: '50rem' }}>
+              <Column field="id" header="#" body={(rowData) => renderEditableCell(rowData, "id")} sortable />
+              <Column field="nome" header="nome" body={(rowData) => renderEditableCell(rowData, "nome")} sortable />
+              <Column header="Ações" body={renderActionCell} />
+            </DataTable>
           </div>
         </div>
       </div>
