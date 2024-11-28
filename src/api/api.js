3a52fb43b0d3aa33e38150183aa71fb00a57a9c0
brunamatuciak <bruna.guedes@sptech.school>
2024-11-28 19:33:43 -0300

    feat: enviando jwt para o back

--- src/api/api.js
+++ src/api/api.js
@@ -1,13 +1,12 @@
 import axios from "axios";
-import { config } from "process";
 
 const api = axios.create({
-    baseURL: "http://localhost:8080/" 
+    baseURL: "http://localhost:8080" 
 });
 
 api.interceptors.request.use(
     (config) => {
-        const token = sessionStorage.getItem('token');
+        const token = localStorage.getItem('token');
 
         if (token){
             config.headers['Authorization'] = `Bearer ${token}`;
