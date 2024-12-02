1d0dd47f234029fce00f192b774742e106ed8ab5
gabrielBmoraes <gabriel.moraes@sptech.school>
2024-12-02 12:40:38 -0800

    ajuste de Loge

--- src/pages/login/LoginPage.jsx
+++ src/pages/login/LoginPage.jsx
@@ -26,12 +26,12 @@ function _alerta(titulo, texto) {
   });
 }
 
-  const Login = () => {
-    const [emailV, setEmail] = useState("");
-    const [senhaV, setSenha] = useState("");
-    let logado = false;
-    let button;
-    let idUsuario;
+const Login = () => {
+  const [emailV, setEmail] = useState("");
+  const [senhaV, setSenha] = useState("");
+  let logado = false;
+  let button;
+  let idUsuario;
 
   const api = axios.create({
     baseURL: "http://localhost:8080/usuarios",
@@ -79,67 +79,69 @@ function _alerta(titulo, texto) {
 
 
 
-    return (
-      <section className="vh-100">
-      <Container fluid>
-        <Row>
-          <Col sm={6} className="px-0 d-none d-sm-block">
-            <img id="login-image" src={loginImage} alt="Login image" className="w-100 vh-100" />
-          </Col>
-          <Col sm={6} className="text-black">
-            <div className="d-flex align-items-center h-custom-2 px-5 pt-5 pt-xl-0 mt-xl-n5 w-100 vh-100">
-              <Form id='login-form' className='w-100'>
-                <h3 className="fw-normal mb-3 pb-3 fw-bold">Entrar</h3>
+      return (
+        <section className="vh-100">
+          <Container fluid>
+            <Row>
+              <Col sm={6} className="px-0 d-none d-sm-block">
+                <img id="login-image" src={loginImage} alt="Login image" className="w-100 vh-100" />
+              </Col>
+              <Col sm={6} className="text-black">
+                <div className="d-flex align-items-center h-custom-2 px-5 pt-5 pt-xl-0 mt-xl-n5 w-100 vh-100">
+                  <Form id='login-form' className='w-100'>
+                    <h3 className="fw-normal mb-3 pb-3 fw-bold">Entrar</h3>
 
-                <Form.Group className="mb-4" controlId="iptEmail">
-                  <Form.Label>Insira seu e-mail:</Form.Label>
-                  <Form.Control
-                    type="email"
-                    placeholder="ana.santos@itapora.org"
-                    size="lg"
-                    onChange={e => setEmail(e.target.value)}
-                  />
-                </Form.Group>
+                    <Form.Group className="mb-4" controlId="iptEmail">
+                      <Form.Label>Insira seu e-mail:</Form.Label>
+                      <Form.Control
+                        type="email"
+                        placeholder="ana.santos@itapora.org"
+                        size="lg"
+                        onChange={e => setEmail(e.target.value)}
+                      />
+                    </Form.Group>
 
-                <Form.Group className="mb-4" controlId="iptSenha">
-                  <Form.Label>Senha:</Form.Label>
-                  <Form.Control
-                    type="password"  // Alterado para password para maior segurança
-                    placeholder="***********"
-                    size="lg"
-                    onChange={e => setSenha(e.target.value)}
-                  />
-                </Form.Group>
+                    <Form.Group className="mb-4" controlId="iptSenha">
+                      <Form.Label>Senha:</Form.Label>
+                      <Form.Control
+                        type="password"  // Alterado para password para maior segurança
+                        placeholder="***********"
+                        size="lg"
+                        onChange={e => setSenha(e.target.value)}
+                      />
+                    </Form.Group>
 
-                <Form.Group className="form-check mb-4 dis d-flex justify-content-between" controlId="esqueceuSenha">
-                  <Form.Check type="checkbox" label="Lembrar de mim" />
+                    <Form.Group className="form-check mb-4 dis d-flex justify-content-between" controlId="esqueceuSenha">
+                      <Form.Check type="checkbox" label="Lembrar de mim" />
 
-                  <p className="small mb-5 pb-lg-2">
-                    <a className={style.redefinirSenha} href="/redefinir-senha">Esqueceu a senha?</a>
-                  </p>
+                      <p className="small mb-5 pb-lg-2">
+                        <a className={style.redefinirSenha} href="/redefinir-senha">Esqueceu a senha?</a>
+                      </p>
 
-                </Form.Group>
+                    </Form.Group>
 
-                <div className="pt-1 mb-4 row">
-                  <Button
-                    className="btn btn-warning btn-lg btn-block text-light fw-bold"
-                    type="button"
-                    onClick={handleLogin}
-                  >
-                    Entrar
-                  </Button>
+                    <div className="pt-1 mb-4 row">
+                      <Button
+                        className="btn btn-warning btn-lg btn-block text-light fw-bold"
+                        type="button"
+                        onClick={handleLogin}
+                      >
+                        Entrar
+                      </Button>
+                    </div>
+
+                    <div className="pt-1 mb-4 row">
+                    </div>
+                  </Form>
                 </div>
+              </Col>
+            </Row>
+          </Container>
+        </section>
 
-                <div className="pt-1 mb-4 row">
-                </div>
-              </Form>
-            </div>
-          </Col>
-        </Row>
-      </Container>
-    </section>
-
-    );
+      );
+    }
   }
+}
 
-  export default Login;
+export default Login;
