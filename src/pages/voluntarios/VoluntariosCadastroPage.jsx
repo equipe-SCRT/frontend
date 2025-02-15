f2b8aca4fae2be7535db48201127c0a3c3a8c864
KauanCursino <kauan.souza@sptech.school>
2025-02-15 19:36:51 -0300

    refactor: aparecendo administrador e voluntário em tipoUsuario na tabela de voluntários

--- src/pages/voluntarios/VoluntariosCadastroPage.jsx
+++ src/pages/voluntarios/VoluntariosCadastroPage.jsx
@@ -19,45 +19,19 @@ const VoluntariosCadastro = () => {
     handleVoluntarios()
   }, [])
 
-  var lista = [];
-
   async function handleVoluntarios() {
     try {
       let encontrados = await api.get("/usuarios");
-
-      for (var i = 0; i < encontrados.data.length; i++) {
-
-        lista.push(
-          <tr>
-            <td className="py-1">
-              {encontrados.data[i].id}
-            </td>
-            <td>
-              {encontrados.data[i].nome}
-            </td>
-            <td>
-              {encontrados.data[i].email}
-            </td>
-            <td>
-              {encontrados.data[i].tipoUsuario == 1 ? "Administrador" : "Voluntário"}
-            </td>
-            <td>
-              <svg value={encontrados.data[i].id} onClick={(e) => handleDelete(e.target.value)}
-                xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="m370-80-16-128q-13-5-24.5-12T307-235l-119 50L78-375l103-78q-1-7-1-13.5v-27q0-6.5 1-13.5L78-585l110-190 119 50q11-8 23-15t24-12l16-128h220l16 128q13 5 24.5 12t22.5 15l119-50 110 190-103 78q1 7 1 13.5v27q0 6.5-2 13.5l103 78-110 190-118-50q-11 8-23 15t-24 12L590-80H370Zm70-80h79l14-106q31-8 57.5-23.5T639-327l99 41 39-68-86-65q5-14 7-29.5t2-31.5q0-16-2-31.5t-7-29.5l86-65-39-68-99 42q-22-23-48.5-38.5T533-694l-13-106h-79l-14 106q-31 8-57.5 23.5T321-633l-99-41-39 68 86 64q-5 15-7 30t-2 32q0 16 2 31t7 30l-86 65 39 68 99-42q22 23 48.5 38.5T427-266l13 106Zm42-180q58 0 99-41t41-99q0-58-41-99t-99-41q-59 0-99.5 41T342-480q0 58 40.5 99t99.5 41Zm-2-140Z" /></svg>
-            </td>
-          </tr>
-        )
-      }
       setVoluntarios(encontrados.data.map((e) => {
         return {
           "id": e.id,
           "nome": e.nome,
           "email": e.email,
-          "tipoUsuario": e.tipoUsuario
+          "tipoUsuario": e.tipoUsuario === 1 ? "Administrador" : "Voluntário"
         }
 
       }))
-      lista = []
+      console.log(encontrados.data)
     } catch (err) {
     }
 
@@ -156,7 +130,7 @@ const VoluntariosCadastro = () => {
       id: rowData.id,
       nome: rowData.nome,
       email: rowData.email,
-      tipoUsuario: rowData.tipoUsuario === "Administrador" ? 1 : 0
+      tipoUsuario: rowData.tipoUsuario === "Administrador" ? 1 : 2
     }
 
     api.patch("/usuarios/atualizar-usuario", usuarioNovo)
@@ -386,7 +360,6 @@ const VoluntariosCadastro = () => {
                 <Column field="nome" header="Nome" body={(rowData) => renderEditableCell(rowData, 'nome')} sortable style={{ padding: '10px' }}>
 
                 </Column>
-                {voluntarios.forEach(x => x.tipoUsuario === 1 ? x.tipoUsuario = "Administrador" : "Voluntário")}
                 <Column field="email" header="Email" body={(rowData) => renderEditableCell(rowData, 'email')} sortable style={{ padding: '10px' }}>
 
                 </Column>
